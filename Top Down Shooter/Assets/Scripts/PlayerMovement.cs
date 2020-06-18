e35ab6550b584a2dd5dbc5b810d35295732a9d23
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-06-17 19:23:38 -0700

    Added Player Movement and Look Controls

---
+++ Top Down Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    private Rigidbody myRigidbody;
+
+    // Input Data for player movement
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    // Reference to camera for player look controls
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For player movement
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * movementSpeed;
+
+        // For player look controls
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+
+    // FixedUpdate is used for physics
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
