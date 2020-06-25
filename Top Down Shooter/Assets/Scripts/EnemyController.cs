9911df6788970ba4eb2df60707d0da5205bf0526
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-06-25 16:11:57 -0700

    Added Enemy Type, Enemy Movement and Look Controls

---
+++ Top Down Shooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    // Reference to enemy Rigidbody
+    private Rigidbody rb;
+
+    // Movement speed of enemy
+    public float moveSpeed;
+
+    // Reference to player
+    public PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.forward * moveSpeed;
+    }
+}
