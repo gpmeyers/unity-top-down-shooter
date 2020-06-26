efa85314424a30ff0df59f92429ee24d194fd821
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-06-26 16:43:44 -0700

    Added Bullet Damage to Enemies

--- Top Down Shooter/Assets/Scripts/BulletController.cs
+++ Top Down Shooter/Assets/Scripts/BulletController.cs
@@ -7,9 +7,30 @@ public class BulletController : MonoBehaviour
 
     public float speed;
 
+    public float lifeTime;
+
+    public int bulletDamage;
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // Logic for despawning bullets after a specified amount of time to save memory
+
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealthManager>().DamageEnemy(bulletDamage);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Top Down Shooter/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    // Fields for enemy max health and current health
+    public int health;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Death Mechanic
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Function allows the player to damage enemies
+    public void DamageEnemy(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
