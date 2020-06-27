5e34ad49b4aad66eff7046a8a6fc6d92107c20a6
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-06-27 01:30:12 -0700

    Added Mechanic for Enemy to Damage Player

---
+++ Top Down Shooter/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+
+    // Fields for the player's health
+    public int health;
+    private int currentHealth;
+
+    // Fields for the length that the player will flash when receiving damage
+    public float flashLength;
+    private float flashCounter;
+
+    private Renderer rend;
+    private Color storedColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = health;
+        rend = GetComponent<Renderer>();
+        storedColor = rend.material.GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Logic to kill the player
+        if(currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        // Logic to have the player flash when hit for a short amount of time
+        if(flashCounter > 0)
+        {
+            flashCounter -= Time.deltaTime;
+
+            if(flashCounter <= 0)
+            {
+                rend.material.SetColor("_Color", storedColor);
+            }
+        }
+    }
+
+    // Function is called whenever the player collides with something that will cause damage
+    public void DamagePlayer(int damage)
+    {
+        currentHealth -= damage;
+
+        // Logic to have the player flash white when damaged
+        flashCounter = flashLength;
+        rend.material.SetColor("_Color", Color.white);
+    }
+}
