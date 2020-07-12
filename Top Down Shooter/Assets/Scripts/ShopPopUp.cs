b36018f9c54e1d3c1b841b373677d4fe0681742d
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-07-12 13:15:04 -0700

    Added A Pop-Up Message to Open the Shop
    TODO: Need to Add UI for shop that opens when the player interacts with the pop up

---
+++ Top Down Shooter/Assets/Scripts/ShopPopUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopPopUp : MonoBehaviour
+{
+
+    public Transform player;
+
+    public GameObject shopPopUpUI;
+
+    private bool isActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isActive && player.position.x <= 21 && player.position.x >= 14 && player.position.z <= -30 && player.position.z >= -38)
+        {
+            shopPopUpUI.SetActive(true);
+            isActive = true;
+        }
+        if (isActive && player.position.x >= 21 || player.position.x <= 14 || player.position.z >= -30 || player.position.z <= -38)
+        {
+            shopPopUpUI.SetActive(false);
+            isActive = false;
+        }
+    }
+}
